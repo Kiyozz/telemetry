41401f4e282ebb8118d2d3ee3c022a25ea72536e
Kévin TURMEL <kevinturmel@gmail.com>
2021-04-06 21:24:50 +0200

    Update cache-clear.ts to not use redis flushdb

--- scripts/cache-clear.ts
+++ scripts/cache-clear.ts
@@ -2,7 +2,11 @@ import cache from '../helpers/cache'
 
 async function start() {
   try {
-    await cache.reset()
+    for (const app of JSON.parse(await cache.getApps()).apps) {
+      await cache.deleteAppViaKey(app.key)
+    }
+
+    await cache.deleteAllApps()
   } catch (e) {
     console.error(e)
     process.exit(1)
